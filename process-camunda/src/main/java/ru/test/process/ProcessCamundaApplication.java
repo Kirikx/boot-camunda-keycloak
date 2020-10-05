15180185da3542648a5e7fdcdbaa64f4cf0c147c
kirill <kvsemenov@fintech.ru>
2020-10-05 09:39:29 +0300

    Initial commit

---
+++ process-camunda/src/main/java/ru/test/process/ProcessCamundaApplication.java
@@ -0,0 +1,11 @@
+package ru.test.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessCamundaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessCamundaApplication.class, args);
+	}
+}
